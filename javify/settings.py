277605ede478f532511d2a1b0366e3f2337560ff
RajasreeFT <rajasreefuturetech@gmail.com>
2025-10-23 15:47:43 +0530

    latest

--- javify/settings.py
+++ javify/settings.py
@@ -171,7 +171,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Optional: Additional folders where Django looks for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
